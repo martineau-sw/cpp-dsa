3a0a24f011522e837fa2f086e8d731b1b0e389de
Seth Martineau <martineausw@gmail.com>
2024-07-10 03:19:23 -0400

    Complete class definition for Queue

--- src/queue/Queue.hpp
+++ src/queue/Queue.hpp
@@ -6,8 +6,31 @@
 namespace martineausw {
 namespace dsa {
 
+template <class T>
 class Queue {
+  public:
+    Queue();
+    ~Queue() = default;
 
+    void enqueue(T);
+    std::unique_ptr<T> dequeue();
+
+    const T* peak_front() const;
+    const T* peak_back() const;
+    int get_length() const;
+  private:
+    class Node {
+      public:
+        Node(T);
+        ~Node() = default;
+        T value;
+        std::unique_ptr<Queue<T>::Node> next;
+      private:
+    }; // class Node
+
+    std::unique_ptr<Queue<T>::Node> front;
+    Queue<T>::Node* back;
+    int length;
 }; // class Queue
 
 } // namespace martineausw
